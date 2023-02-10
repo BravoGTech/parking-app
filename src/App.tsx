d2ea13830671098864e42d44e66dd4eebf2b27a8
Igor <igorgg@gmail.com>
2023-02-10 18:04:37 -0300

    feat: design system

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <Heading size={"4xl"} lineHeight="4xl">
+        hello world
+      </Heading>
+      <Heading>hello world</Heading>
+      <Heading>hello world</Heading>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { queryClient } from "./services/queryClient";
+import { theme } from "./styles/theme";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
